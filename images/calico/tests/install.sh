9dfbcaf7142d6bac9d585ca77c683159292efe83
Jason Hall <jason@chainguard.dev>
2023-07-03 15:40:39 -0400

    use tmpdir for install test
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/calico/tests/install.sh
+++ images/calico/tests/install.sh
@@ -4,6 +4,9 @@
 
 set -o errexit -o nounset -o errtrace -o pipefail -x
 
+TMP="$(mktemp -d)"
+pushd "${TMP}"
+
 curl -o calico.yaml https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/calico.yaml
 
 sed -i.bak "s|docker.io/calico/node:.*|${NODE_IMAGE}|g" calico.yaml
