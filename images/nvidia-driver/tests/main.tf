a2bed68d8bd64e81b4d0293844ef8050f7c81be8
Jason Hall <jason@chainguard.dev>
2023-12-18 11:32:35 -0500

    .
    Signed-off-by: Jason Hall <jason@chainguard.dev>

---
+++ images/nvidia-driver/tests/main.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    oci = { source = "chainguard-dev/oci" }
+  }
+}
+
+variable "digest" {
+  description = "The image digest to run tests over."
+}
+
+// TODO tests
